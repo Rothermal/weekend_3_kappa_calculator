62cee8482f2f11b395c87da72f5745b640e4882c
Mark Rothermal <rothermal2@comcast.net>
2016-03-12 18:29:27 -0600

    wheee

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var app = express();
-var index = require('./routes/index');
 var bodyParser = require('body-parser');
+var index = require('./routes/index');
+var heroku =  process.env.PORT || 3000;
+
+
 app.use(express.static('server/public'));
 
 app.use(bodyParser.json());
@@ -10,7 +13,7 @@ app.use(bodyParser.urlencoded({extended : true}));
 
 app.use('/',index);
 
-var server = app.listen(3000,function(){
+var server = app.listen(heroku,function(){
    var port = server.address().port;
     console.log('server up on port : ', port);
 });
